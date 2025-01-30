b288f16c4ef3914dc131410d447be4609458ae1e
Josimar Reyes Campos <darth_jos@hotmail.com>
2025-01-30 13:43:13 -0600

    Curso Geekipedia | Vid 5 | Añadido ejemplo @GetMapping

--- src/main/java/cursoSpringBoot/controllers/HelloWorldRestController.java
+++ src/main/java/cursoSpringBoot/controllers/HelloWorldRestController.java
@@ -1,7 +1,14 @@
 package cursoSpringBoot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldRestController {
+
+    @GetMapping({"/holaMundo", "/helloWorld", "/hw"})
     public String helloWorld() {
+        System.out.println("helloWorld() --> Solicitud ejecutada!");
         return "Hola Mundo!";
     }
 }
